e7769d6a05e3bce6aedafeb96a133d865fdd428d
Daniela Manjarres Virviescas <danielamvirviescas2004@gmail.com>
2023-05-07 19:06:01 -0500

    Modificacion de archivo jsx y creacion de duplicado para posteriormente modificarlo.DMV

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import ProjectsPage from './projects/ProyectsPage';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <blockquote cite="MONTESQUIEU">
-    +Hay que estudiar mucho para saber poco.
-    +   </blockquote>
-  );
+ return(
+   <div className='container'>
+       <ProjectsPage></ProjectsPage>
+   </div>
+ );
 }
 
 export default App;

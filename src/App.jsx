0849118b6af9ecbc0c5541186899cdcd41eb4810
Daniela Manjarres Virviescas <danielamvirviescas2004@gmail.com>
2023-05-07 19:34:34 -0500

    Modificacion de archivo Projectspage2.jsx.DMV

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import ProjectsPage from './projects/ProyectsPage';
+import ProjectsPage2 from './projects/ProjectsPage2';
 import './App.css'
 
 function App() {
  return(
    <div className='container'>
        <ProjectsPage></ProjectsPage>
+       <ProjectsPage2></ProjectsPage2>
    </div>
  );
 }
---
+++ src/projects/ProjectsPage2.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ProyectsPage2 = () => {
+    const name ="Daniela";
+    const element = <h1>Hello,
+         { name }. Welcome to aprendices de React</h1>
+    return element
+}
+
+export default ProyectsPage2;

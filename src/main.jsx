9a9bc448b364135e1ba52ffb63537158e1ef1476
Daniela Manjarres Virviescas <danielamvirviescas2004@gmail.com>
2023-05-07 19:58:37 -0500

    Modificacion de archivo Projectspage2.jsx.DMV

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Componente1 from "./projects/componente1.jsx";
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,5 +10,3 @@ root.render(
     <Componente1></Componente1>
   </React.StrictMode>
 );
-
-reportWebVitals();

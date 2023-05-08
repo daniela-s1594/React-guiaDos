dae6c6f97e4c09c1910ab146cab5a25133dea638
Daniela Manjarres Virviescas <danielamvirviescas2004@gmail.com>
2023-05-07 19:00:09 -0500

    Creacion de archivo React e instalacion de paquetes,creacion de carpeta projects, creacion de archivo jsx y modificacion en oros archivos.DMV

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <blockquote cite="MONTESQUIEU">
+    +Hay que estudiar mucho para saber poco.
+    +   </blockquote>
+  );
+}
+
+export default App;
